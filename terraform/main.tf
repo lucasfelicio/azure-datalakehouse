78be233bc709c8d2fabb129e1102335646f9425c
lucasfelicio <lucas.felicio@credisis.com.br>
2024-08-08 18:44:57 -0300

    feat: edit main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,85 +13,115 @@ provider "azurerm" {
 
 # Define locals
 locals {
-  postfixed = "lsflakehouse"
+  prod_fixed = "lucas-prod"
+  dev_fixed = "lucas-dev"
 }
 
-# Deploy Resource Group
-resource "azurerm_resource_group" "rg" {
-    name = "rg-${local.postfixed}"
+# Deploy Resources Groups
+resource "azurerm_resource_group" "rg_prod" {
+    name = "rg-${local.prod_fixed}"
     location = var.region
-  
+}
+
+resource "azurerm_resource_group" "rg_dev" {
+    name = "rg-${local.dev_fixed}"
+    location = var.region
 }
 
 # Deploy Azure Key Vault
-resource "azurerm_key_vault" "akv" {
-  name = "akv-${local.postfixed}"
+resource "azurerm_key_vault" "akv_prod" {
+  name = "akv-${local.prod_fixed}"
   location = var.region
-  resource_group_name = azurerm_resource_group.rg.name
+  resource_group_name = azurerm_resource_group.rg_prod.name
   sku_name = "standard"
   tenant_id = data.azurerm_client_config.current.tenant_id
+}
 
+resource "azurerm_key_vault" "akv_dev" {
+  name = "akv-${local.dev_fixed}"
+  location = var.region
+  resource_group_name = azurerm_resource_group.rg_dev.name
+  sku_name = "standard"
+  tenant_id = data.azurerm_client_config.current.tenant_id
 }
 
 # Deploy Azure Data Factory Production
-resource "azurerm_data_factory" "adf-prod" {
-  name = "adf-${local.postfixed}-prod"
-  resource_group_name = azurerm_resource_group.rg.name
+resource "azurerm_data_factory" "adf_prod" {
+  name = "adf-${local.prod_fixed}"
+  resource_group_name = azurerm_resource_group.rg_prod.name
   location = var.region  
 }
 
 # Deploy Azure Data Factory Develop
-resource "azurerm_data_factory" "adf-dev" {
-  name = "adf-${local.postfixed}-dev"
-  resource_group_name = azurerm_resource_group.rg.name
+resource "azurerm_data_factory" "adf_dev" {
+  name = "adf-${local.dev_fixed}"
+  resource_group_name = azurerm_resource_group.rg_dev.name
   location = var.region  
 }
 
 # Deploy Azure Storage Account
-# resource "azurerm_storage_account" "adls" {
-#   name = "adls${local.postfixed}"
-#   resource_group_name = azurerm_resource_group.rg.name
-#   location = var.region
-#   account_tier = "Standard"
-#   account_replication_type = "LRS"
-#   is_hns_enabled = true
-  
-# }
+resource "azurerm_storage_account" "adls_prod" {
+  name = "adlslucasdev"
+  resource_group_name = azurerm_resource_group.rg_prod.name
+  location = var.region
+  account_tier = "Standard"
+  account_replication_type = "LRS"
+  is_hns_enabled = true
+}
+
+resource "azurerm_storage_account" "adls_dev" {
+  name = "adlslucasprod"
+  resource_group_name = azurerm_resource_group.rg_dev.name
+  location = var.region
+  account_tier = "Standard"
+  account_replication_type = "LRS"
+  is_hns_enabled = true
+}
 
 # Deploy Containers Data Lake
-# resource "azurerm_storage_container" "containerlanding" {
-#   name = "landing-zone"
-#   storage_account_name = azurerm_storage_account.adls.name
-#   container_access_type = "private"
-  
-# }
+resource "azurerm_storage_container" "container_landing_zone" {
+  name = "landing-zone"
+  storage_account_name = azurerm_storage_account.adls_prod.name
+  container_access_type = "private"
+}
 
-# resource "azurerm_storage_container" "containerbronze" {
-#   name = "bronze"
-#   storage_account_name = azurerm_storage_account.adls.name
-#   container_access_type = "private"
-  
-# }
+resource "azurerm_storage_container" "container_bronze" {
+  name = "bronze"
+  storage_account_name = azurerm_storage_account.adls_prod.name
+  container_access_type = "private"
+}
 
-# resource "azurerm_storage_container" "containersilver" {
-#   name = "silver"
-#   storage_account_name = azurerm_storage_account.adls.name
-#   container_access_type = "private"
-  
-# }
+resource "azurerm_storage_container" "container_silver" {
+  name = "silver"
+  storage_account_name = azurerm_storage_account.adls_prod.name
+  container_access_type = "private"
+}
 
-# resource "azurerm_storage_container" "containergold" {
-#   name = "gold"
-#   storage_account_name = azurerm_storage_account.adls.name
-#   container_access_type = "private"
-  
-# }
+resource "azurerm_storage_container" "container_gold" {
+  name = "gold"
+  storage_account_name = azurerm_storage_account.adls_prod.name
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_container" "container_dev" {
+  name = "dev"
+  storage_account_name = azurerm_storage_account.adls_dev.name
+  container_access_type = "private"
+}
 
 # Deploy Azure Databricks
-# resource "azurerm_databricks_workspace" "databricks" {
-#   name = "adb-${local.postfixed}"
-#   resource_group_name = azurerm_resource_group.rg.name
-#   location = var.region
-#   sku = "premium"
-#   managed_resource_group_name = "rg-adb-${local.postfixed}"  
-# }
+resource "azurerm_databricks_workspace" "databricks_prod" {
+  name = "adb-${local.prod_fixed}"
+  resource_group_name = azurerm_resource_group.rg_prod.name
+  location = var.region
+  sku = "premium"
+  managed_resource_group_name = "rg-adb-${local.prod_fixed}"  
+}
+
+resource "azurerm_databricks_workspace" "databricks_dev" {
+  name = "adb-${local.dev_fixed}"
+  resource_group_name = azurerm_resource_group.rg_dev.name
+  location = var.region
+  sku = "premium"
+  managed_resource_group_name = "rg-adb-${local.dev_fixed}"  
+}
